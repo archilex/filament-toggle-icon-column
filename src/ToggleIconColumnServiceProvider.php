54ed81dfce99f6b68c77cdbf1e708f9bbf365616
Kenneth Sese <ksese@hope4tomorrow.net>
2023-05-09 11:52:13 -0600

    Update ToggleIconColumnServiceProvider.php

--- src/ToggleIconColumnServiceProvider.php
+++ src/ToggleIconColumnServiceProvider.php
@@ -7,10 +7,6 @@ use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class ToggleIconColumnServiceProvider extends PackageServiceProvider
 {
-    protected array $styles = [
-        'plugin-filament-toggle-icon-column' => __DIR__.'/../resources/dist/filament-toggle-icon-column.css',
-    ];
-
     public function configurePackage(Package $package): void
     {
         $package->name('filament-toggle-icon-column')
