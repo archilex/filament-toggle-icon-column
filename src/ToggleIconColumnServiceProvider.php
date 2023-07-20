c7cbc1a96e10ef26ab1495b34ea84193c8ea44e1
Kenneth Sese <ksese@hope4tomorrow.net>
2023-07-19 22:32:33 -0600

    WIP

--- src/ToggleIconColumnServiceProvider.php
+++ src/ToggleIconColumnServiceProvider.php
@@ -4,6 +4,8 @@ namespace Archilex\ToggleIconColumn;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Filament\Support\Facades\FilamentAsset;
+use Filament\Support\Assets\Css;
 
 class ToggleIconColumnServiceProvider extends PackageServiceProvider
 {
@@ -12,5 +14,12 @@ class ToggleIconColumnServiceProvider extends PackageServiceProvider
         $package->name('filament-toggle-icon-column')
             ->hasAssets()
             ->hasViews();
+    }
+
+    public function packageBooted(): void
+    {
+        FilamentAsset::register([
+            Css::make('filament-toggle-icon-column', __DIR__.'/../resources/dist/filament-toggle-icon-column.css'),
+        ], 'archilex/filament-toggle-icon-column');
     }
 }
